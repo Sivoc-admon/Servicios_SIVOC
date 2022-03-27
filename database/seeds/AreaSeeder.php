2dcc0c6bc1c9704c06c201fec7644b69027525b2
Francisco <jfgils02@gmail.com>
2022-03-26 19:01:23 -0600

    se modifican areas y roles

--- database/seeds/AreaSeeder.php
+++ database/seeds/AreaSeeder.php
@@ -13,21 +13,18 @@ class AreaSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['name' => 'Almacen', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
+
             ['name' => 'Calidad', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
-            ['name' => 'Operaciones', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
+            ['name' => 'Tesoreria', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Compras', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Direccion', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
-            ['name' => 'Finanzas', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
-            ['name' => 'Ingenieria', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
-            ['name' => 'Manufactura', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Recursos Humanos', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Ventas', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')],
             ['name' => 'Servicio', 'created_at' => date('Y-m-d H:m:s'), 'updated_at' => date('Y-m-d H:m:s')]
         ];
 
         DB::table('areas')->insert($data);
-        
-        
+
+
     }
 }
